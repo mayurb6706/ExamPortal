9a32be0f76292688b3feac94648fb6ebb6f981b3
mayurb6706 <mayurbhosale6706@gmail.com>
2024-06-26 23:01:44 +0530

    create a form module form sign up template

--- Exam-Portal-Frontend/src/app/auth/sign-up/sign-up.component.ts
+++ Exam-Portal-Frontend/src/app/auth/sign-up/sign-up.component.ts
@@ -1,15 +1,36 @@
 import { Component, OnInit } from '@angular/core';
+import {
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-sign-up',
   templateUrl: './sign-up.component.html',
-  styleUrls: ['./sign-up.component.css']
+  styleUrls: ['./sign-up.component.css'],
 })
 export class SignUpComponent implements OnInit {
-
-  constructor() { }
+  registerForm: FormGroup;
+  constructor(private formBuilder: FormBuilder) {}
 
   ngOnInit(): void {
+    this.registerForm = this.formBuilder.group({
+      username: new FormControl('', [Validators.required]),
+      password: new FormControl('', [Validators.required]),
+      email: new FormControl('', [Validators.required]),
+      contact: new FormControl('', [Validators.required]),
+      firstname: new FormControl('', [Validators.required]),
+      lastname: new FormControl('', [Validators.required]),
+    });
   }
 
+  register(){
+    console.log(this.registerForm.value)
+  }
+
+  clearInput(){
+    this.registerForm.reset()
+  }
 }
