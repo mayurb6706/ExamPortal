86470b2519cdd747087dd8dea8136667c7c26662
Bhosale <mayur.b.bhosale@capgemini.com>
2024-07-03 15:43:03 +0530

    Quiz template updated

--- Exam-Portal-Frontend/src/app/quiz/list-quiz/list-quiz.component.ts
+++ Exam-Portal-Frontend/src/app/quiz/list-quiz/list-quiz.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Quiz } from 'src/app/Model/quiz';
+import { QuizService } from 'src/app/services/quiz.service';
 
 @Component({
   selector: 'app-list-quiz',
@@ -7,9 +9,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListQuizComponent implements OnInit {
 
-  constructor() { }
+  quizes:Quiz[]=[]
+  constructor(private quizService:QuizService) { }
 
   ngOnInit(): void {
+
+    this.quizService.getAllQuiz().subscribe(data=>{
+      console.log(data)
+      this.quizes=data
+    })
+
+  }
+
+  deleteCategeory(id:number){
+    
   }
 
 }
--- Exam-Portal-Frontend/src/app/services/quiz.service.ts
+++ Exam-Portal-Frontend/src/app/services/quiz.service.ts
@@ -7,14 +7,17 @@ import { Quiz } from '../Model/quiz';
   providedIn: 'root'
 })
 export class QuizService {
- 
+  
   private quizUrl="http://localhost:8080/api/quiz"
 
   constructor(private http:HttpClient) { }
 
   createQuiz(quiz:Quiz):Observable<any> {
-    console.log("From Quiz Service "+JSON.stringify(quiz))
-
   return this.http.post<any>(`${this.quizUrl}/create`,quiz);
   }
+
+  getAllQuiz():Observable<any[]> {
+    return this.http.get<any[]>(`${this.quizUrl}/all`)
+   }
+  
 }
